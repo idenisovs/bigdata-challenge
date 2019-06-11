a660cc276ca27f1e9038de5e14fe539a36f5a1a2
Ilya Denisov <i.denisovs@gmail.com>
2019-06-11 11:37:40 +0300

    Read location from CLI arguments.

--- src/main/scala/device/Main.scala
+++ src/main/scala/device/Main.scala
@@ -8,8 +8,15 @@ import scala.io.Source
 import device.dto.Location
 
 object Main extends App {
+  val locations: Map[String, Location] = Map(
+    "Jekabpils" -> new Location(latitude = 56.499444, longitude = 25.878333),
+    "Riga" -> new Location(latitude = 56.9475, longitude = 24.106389),
+    "Jelgava" -> new Location(latitude = 56.648333, longitude = 23.713889),
+    "Dobele" -> new Location(latitude = 56.616667, longitude = 23.266667)
+  )
+
   val UUID_FILE_NAME = "uuid.txt"
-  val location = new Location(latitude = 56.616667, longitude = 23.266667)
+  val location = getLocation(args(0))
   val device = new Device(id = readUUID(), location = location)
 
   val timer = new Timer
@@ -37,5 +44,9 @@ object Main extends App {
     val uuid = Source.fromFile(UUID_FILE_NAME).mkString
 
     UUID.fromString(uuid)
+  }
+
+  def getLocation(city: String): Location = {
+    locations.getOrElse(city, locations("Riga"))
   }
 }
